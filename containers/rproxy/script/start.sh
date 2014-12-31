af3a94acd67e70693454295bef2e4f4921250a87
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-12-31 23:24:15 +0900

    Make my containers fleetctl-able

--- containers/rproxy/script/start.sh
+++ containers/rproxy/script/start.sh
@@ -5,7 +5,7 @@ NGINX_CONF=/etc/nginx/conf.d/default.conf
 
 sed -e "s!SED_PORTFOLIO_URL!http://${PORTFOLIO_PORT_80_TCP_ADDR}!g" \
 	-e "s!SED_BLOG_URL!http://${BLOG_PORT_80_TCP_ADDR}!g" \
-	-e "s!SED_JPJOURNAL_URL!http://${JPJOURNAL_PORT_80_TCP_ADDR}!g" \
+	-e "s!SED_JP_URL!http://${JP_PORT_80_TCP_ADDR}!g" \
 	< $NGINX_CONF_TEMPLATE > $NGINX_CONF
 
 nginx -g 'daemon off;'

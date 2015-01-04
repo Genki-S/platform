e297cd0a5529095a1e0e601853bcbe0abfc361a0
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-01-04 10:58:32 +0900

    Use ambassodor pattern to make my services host-independent

--- containers/rproxy/script/start.sh
+++ containers/rproxy/script/start.sh
@@ -3,9 +3,9 @@
 NGINX_CONF_TEMPLATE=/etc/nginx/conf.d/default.conf.tmpl
 NGINX_CONF=/etc/nginx/conf.d/default.conf
 
-sed -e "s!SED_PORTFOLIO_URL!http://${PORTFOLIO_PORT_80_TCP_ADDR}!g" \
-	-e "s!SED_BLOG_URL!http://${BLOG_PORT_80_TCP_ADDR}!g" \
-	-e "s!SED_JP_URL!http://${JP_PORT_80_TCP_ADDR}!g" \
+sed -e "s!SED_PORTFOLIO_URL!http://${COREOS_PRIVATE_IPV4}:${PORTFOLIO_PORT}!g" \
+	-e "s!SED_BLOG_URL!http://${COREOS_PRIVATE_IPV4}:${BLOG_PORT}!g" \
+	-e "s!SED_JP_URL!http://${COREOS_PRIVATE_IPV4}:${JP_PORT}!g" \
 	< $NGINX_CONF_TEMPLATE > $NGINX_CONF
 
 nginx -g 'daemon off;'
